c342df119487132d425fb539e7b0e8c925bd4948
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-08-27 09:32:22 +0530

    fix: Use Joi to validate input

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -18,11 +19,15 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
-    //400 Bad request
-    res.status(400).send("Name is required and should be minimum 3 chars");
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
     return;
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
