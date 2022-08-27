28a5dca377b18a4eb9e87c55590ed915d1d35873
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-08-27 11:16:28 +0530

    feat: Use morgan middleware to log http rquests

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true })); //parses incoming requests with
 app.use(express.static("public"));
 app.use(logger);
 app.use(helmet());
-app.use(morgan());
+app.use(morgan("tiny"));
 
 app.use(authenticate);
 const courses = [
